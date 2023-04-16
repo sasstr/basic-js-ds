1247dd5e9b356a8429ffa3cad43207e606beb3c5
Alexander Strakhovenko <sasstr@gmail.com>
2023-04-16 15:27:04 +0300

    feat: implement clases queue and stack

--- src/queue.js
+++ src/queue.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
@@ -8,28 +8,36 @@ const { NotImplementedError } = require('../extensions/index.js');
  * @example
  * const queue = new Queue();
  *
- * queue.enqueue(1); // adds the element to the queue
- * queue.enqueue(3); // adds the element to the queue
- * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
+ * queue.enqueue(1); // adds the value to the queue
+ * queue.enqueue(3); // adds the value to the queue
+ * queue.dequeue(); // returns the top value from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
 class Queue {
+  node = new ListNode();
 
   getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    return this.node;
+  };
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+  enqueue(value) {
+    let endNode = this.node;
+    while (endNode.next !== null) {
+      endNode = endNode.next;
+    }
+    if (endNode.value === undefined) {
+      endNode.value = value;
+    } else {
+      endNode.next = new ListNode(value);
+    }
+  };
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-}
+    const value = this.node.value;
+    this.node = this.node.next;
+    return value;
+  };
+};
 
 module.exports = {
   Queue
